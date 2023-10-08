9db96892eddb4faada52e87a3e698a5eae48799b
Olivier <olipt.mignot@gmail.com>
2023-10-08 22:02:24 +0200

    run build with telefoot project

---
+++ dist/assets/ProjectDescription-7c2fc3e3.js
@@ -0,0 +1,1 @@
+import{u as e}from"./ProjectsStore-813ad14d.js";import{_ as o,o as t,c}from"./index-f525c6bc.js";const s={class:"container"},r={__name:"ProjectDescription",setup(_){return e().getProjects,(a,p)=>(t(),c("main",s," Bonjour "))}},j=o(r,[["__scopeId","data-v-0484afee"]]);export{j as default};
