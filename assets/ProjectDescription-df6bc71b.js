e3071f787bde905f0d4a96072e4d306f0b1af650
Olivier <olipt.mignot@gmail.com>
2023-11-26 16:07:22 +0100

    run build witch Interstellar Adventures project

--- assets/ProjectDescription-df6bc71b.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./ProjectsStore-dbf68244.js";import{_ as o,o as t,c}from"./index-d4733f2d.js";const s={class:"container"},r={__name:"ProjectDescription",setup(_){return e().getProjects,(a,p)=>(t(),c("main",s," Bonjour "))}},j=o(r,[["__scopeId","data-v-0484afee"]]);export{j as default};
